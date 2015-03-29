821f5cb124b4f0e929e1b5490b4f7d19ce645533
王雪梅 <wxm112@me.com>
2015-03-29 11:40:11 +1100

    added c3 lib and moved onRequest function to the app object

--- herokumon.js
+++ herokumon.js
@@ -1,6 +1,13 @@
+  var f = function(value) {return Math.floor(value);};
+  var r = function(max) { return Math.random()*max; };
+  var s = function(value) {return Math.sqrt(value);};
+
+
 var app = {
   WIDTH: 1400,
-  HEIGHT: 700,
+  HEIGHT: 400,
+  NUMBER_OF_REQUESTS_TO_SHOW: 200,
+  requests: [],
 
   drawSVG: function () {
     if (!app.svg) {
@@ -10,14 +17,20 @@ var app = {
                         .attr("height", app.HEIGHT);
     }
     return app.svg;
+  },
+
+  onRequest: function(request) {
+    this.requests.push(request);
+    if (this.requests.length > this.NUMBER_OF_REQUESTS_TO_SHOW) {
+      this.requests.shift();
+    };
+    // bars.rects();
   }
 };
 
 var bars = {
-  requests: [],
   MAX_REQUEST_TIME: 2000,
-  NUMBER_OF_REQUESTS_TO_SHOW: 200,
-  MAX_BAR_HEIGHT: 40,
+  MAX_BAR_HEIGHT: 80,
   
 
   heightScale: function () { 
@@ -34,9 +47,9 @@ var bars = {
 
   
   rects: function () {
-    var bar_width = app.WIDTH/bars.NUMBER_OF_REQUESTS_TO_SHOW;
+    var bar_width = app.WIDTH/app.NUMBER_OF_REQUESTS_TO_SHOW;
     var svg = app.drawSVG();
-    var selection = svg.selectAll('rect').data(this.requests);
+    var selection = svg.selectAll('rect').data(app.requests);
 
     selection.enter()
       .append('rect')
@@ -46,63 +59,107 @@ var bars = {
     selection.exit()
       .remove();
 
-    selection
+    // debugger;
+    selection 
       .attr("height", function(d) { return bars.heightScale()(d.service + d.connect); })
       .attr("y", function(d) { return bars.MAX_BAR_HEIGHT - bars.heightScale()(d.service + d.connect); })
       .attr("fill", function(d) { return "rgb(" + Math.floor(bars.colourScale()(d.service + d.connect)) + ", 149, 228)" })
 
-  },
-
-  onRequest: function(request) {
-    this.requests.push(request);
-    if (this.requests.length > bars.NUMBER_OF_REQUESTS_TO_SHOW) {
-      this.requests.shift();
-    };
-    this.rects();
   }
 };
 
-var pies = {
-  dynos: [],
-  outer_r: 50,
-  innter_r: 15,
-  padding: 60,
-  top: 200,
+// var pies = {
+//   dynos: [],
+//   outer_r: 80,
+//   innter_r: 15,
+//   padding: 60,
+//   top: 200,
 
-  circles: function() {
-    var svg = app.drawSVG();
-    var selection = svg.selectAll('circle').data(this.dynos);
-    var pie_space = app.WIDTH/pies.dynos.length;
+//   circles: function() {
+//     var svg = app.drawSVG();
+//     var selection = svg.selectAll('circle').data(this.dynos);
+//     var pie_space = app.WIDTH/pies.dynos.length;
 
-    selection.enter()
-      .append('circle')
-        .attr('cy', pies.top)
-        .attr('r', pies.outer_r)
-        .attr('fill', 'yellow');
+//     selection.enter()
+//       .append('circle')
+//         .attr('cy', pies.top)
+//         .attr('r', pies.outer_r)
+//         .attr('fill', 'yellow');
 
-    selection.exit()
-      .remove();
+//     selection.exit()
+//       .remove();
 
-    selection
-      .attr("cx", function(d,i) {
-        var padding = pie_space/2;
-        return padding + i*pie_space;
-      });
+//     selection
+//       .attr("cx", function(d,i) {
+//         var padding = pie_space/2;
+//         return padding + i*pie_space;
+//       });
+//   },
+//   onRequest: function(request) {
+//     this.dynos.push(request.dyno);
+//     this.dynos = _.uniq(this.dynos)
+//     this.circles();
+//   }
+// };
 
-    
-  },
+// var dynos = {
+//   dynos: {},
+//   getChart: function() {
+//     if(!this.chart) {
+//       this.chart = c3.generate({
+//         bindto: '.donut',
+//         data: {
+//             columns: [
+//                 ['200s/300s', 0],
+//                 ['400s', 0],
+//                 ['500s', 0]
+//             ],
+//             type : 'donut'
+//         },
+//         donut: {
+//             title: "Requests"
+//         }
+//       });
+//     }
+//     return this.chart;
+//   },
+//   draw: function() {
+//     var rOk   = this.status.filter(function(status){ return status >= 200 && status < 400 }).length;
+//     var r400s = this.status.filter(function(status){ return status >= 400 && status < 500 }).length;
+//     var r500s = this.status.filter(function(status){ return status >= 500 && status < 600 }).length;
 
-  onRequest: function(request) {
-    this.dynos.push(request.dyno);
-    this.dynos = _.uniq(this.dynos)
-    this.circles();
-  }
+//     this.getChart().load({
+//         columns: [
+//             ['200s/300s', rOk],
+//             ['400s', r400s],
+//             ['500s', r500s]
+//         ]
+//     });
+//   },
+//   onRequest: function(request) {
+//     // debugger;
+//     var dyno = dynos[request.dyno];
+//     if (!dyno) {
+//       dyno = [];
+//       request.status.push(dyno);
+//     } else {
+//       request.status.push(dyno);
+//     };
+//     if (this.status.length > bars.NUMBER_OF_REQUESTS_TO_SHOW) {
+//       this.status.shift();
+//     };
+//     this.draw();
+//   }
+// };
+
+var onRequestFunction = function(data) {
+  data.dyno = 'fake.' + f(r(5));
+  app.onRequest(data);
+  // pies.onRequest(data);
+  // dynos.onRequest(data);
 };
 
 window.onload = function () {
   var socket = io('https://Herokumon.herokuapp.com');
-  socket.on('request', function(data) {
-    bars.onRequest(data);
-    pies.onRequest(data);
-  });
+  socket.on('request', onRequestFunction);
 };
